4f4627801b684f96280126c7012eb499d128ec6a
ROBERT_FRAMPTON <ROBERT.FRAMPTON@LIFERAY.COM>
2012-12-03 16:03:18 -0800

    Broken Theme Test

--- themes/broken-theme/docroot/_diffs/js/main.js
+++ themes/broken-theme/docroot/_diffs/js/main.js
@@ -1,5 +1,5 @@
 AUI().ready(
-	'liferay-hudcrumbs', 'liferay-navigation-interaction',
+	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'aui-dialog', 'aui-overlay-manager', 'aui-io-request', 'io-plugin', 'aui-dialog-iframe',
 	function(A) {
 		var navigation = A.one('#navigation');
 
@@ -13,11 +13,34 @@ AUI().ready(
 			siteBreadcrumbs.plug(A.Hudcrumbs);
 		}
 
-		A.getBody().delegate('click', eventHandler, 'a.logo');
-
-		var eventHandler function(event){
+		var eventHandler = function(event) {
 			event.preventDefault();
 			alert(event.currentTarget.attr('title'));
 		};
+
+		A.getBody().delegate('click', eventHandler, 'a.logo');
+
+		A.one('#sign-in').on(
+			'click',
+			function(event) {
+				event.preventDefault();
+
+				var dialog1 = new A.Dialog({
+				    title: 'Sign-in',
+				    centered: true,
+				    modal: true,
+				    width: 500,
+				    height: 300,
+				}).render();
+
+				dialog1.plug(
+				A.Plugin.DialogIframe,
+					{
+						uri: '',
+						method: 'POST',
+					}
+				);
+			}
+		);
 	}
 );
