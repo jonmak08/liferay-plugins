ed726fa97fd5e52b2d36d73e5edb3f3089431c29
Patrick Armitage <patrick.armitage@liferay.com>
2013-01-16 16:37:21 -0800

    Add Ajax Sign-in Dialog. Fix Bugs. Parent to Classic Theme

--- themes/broken-theme/docroot/_diffs/js/main.js
+++ themes/broken-theme/docroot/_diffs/js/main.js
@@ -1,5 +1,5 @@
 AUI().ready(
-	'liferay-hudcrumbs', 'liferay-navigation-interaction',
+	'liferay-hudcrumbs', 'liferay-navigation-interaction', 'aui-dialog', 'aui-io-request',
 	function(A) {
 		var navigation = A.one('#navigation');
 
@@ -7,17 +7,37 @@ AUI().ready(
 			navigation.plug(Liferay.NavigationInteraction);
 		}
 
-		var siteBreadcrumb = A.one('.site-breadcrumbs');
+		var siteBreadcrumbs = A.one('.site-breadcrumbs');
 
 		if (siteBreadcrumbs) {
 			siteBreadcrumbs.plug(A.Hudcrumbs);
 		}
 
-		A.getBody().delegate('click', eventHanler, 'a.logo');
-
-		var eventHandler function(event){
+		var signInDialog = function(event) {
 			event.preventDefault();
-			alert(event.currentTarget.attr('title'));
+			var href = event.currentTarget.get('href');
+
+			var signInDialog = new A.Dialog({
+				align: {
+					node: 'a#sign-in',
+					points: [ 'tr', 'br' ]
+				},
+				constrain2view: true,
+				resizable: false,
+				title: 'Sign-in Form'
+			});
+
+			A.io.request(href, {
+				on: {
+					success: function(event, id, xhr) {
+						var ajaxContent = A.Node.create('<div></div>').html(this.get('responseData'));
+						var ajaxSignIn = ajaxContent.one('#portlet_58');
+						signInDialog.set('bodyContent', ajaxSignIn).render();
+					}
+			  }
+			});
 		};
+
+		A.one('body').delegate('click', signInDialog, 'a#sign-in');
 	}
 );
