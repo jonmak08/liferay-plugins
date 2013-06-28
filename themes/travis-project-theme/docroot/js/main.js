cd775331ed5df78263fdb7ab4000481e5c5b98e9
TRAVIS_CORY <travis.cory@liferay.com>
2013-06-28 16:49:45 -0700

    Here are the changes to my theme. I was never able to get the nav bar to work well.s

--- themes/travis-project-theme/docroot/js/main.js
+++ themes/travis-project-theme/docroot/js/main.js
@@ -1,13 +1,27 @@
-AUI().ready(
+function mainmenu(){
+jQuery("#nav ul").css({display: "none"}); // Opera Fix
+jQuery(" #nav li").hover(function(){
+		jQuery(this).find('ul:first:hidden').css({visibility: "visible",display: "none"}).show(200);
+		},function(){
+		jQuery(this).find('ul:first').css({visibility: "hidden"});
+		});
+}
 
-	/*
+ jQuery(document).ready(function(){
+ /*
 	This function gets loaded when all the HTML, not including the portlets, is
 	loaded.
 	*/
+	mainmenu();
+});
 
+/*
+jQuery(document).ready(
+
+	
 	function() {
 	}
-);
+);*/
 
 Liferay.Portlet.ready(
 
@@ -15,18 +29,20 @@ Liferay.Portlet.ready(
 	This function gets loaded after each and every portlet on the page.
 
 	portletId: the current portlet's id
-	node: the Alloy Node object of the current portlet
+	jQueryObj: the jQuery wrapped object of the current portlet
 	*/
 
-	function(portletId, node) {
+	function(portletId, jQueryObj) {
 	}
 );
 
-Liferay.on(
-	'allPortletsReady',
+jQuery(document).last(
 
-	
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
 	function() {
 	}
 );
-
