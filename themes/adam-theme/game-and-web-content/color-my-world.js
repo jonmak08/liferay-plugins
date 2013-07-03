a866aecc32097068027e924ac573fc659cb17663
Adam Parower <adam.parower@liferay.com>
2013-07-03 12:00:29 -0700

    now allows user to choose board size and number of colors

--- themes/adam-theme/game-and-web-content/color-my-world.js
+++ themes/adam-theme/game-and-web-content/color-my-world.js
@@ -1,25 +1,48 @@
-var size = 14;
-var moves = 0;
+var size;
+var moves;
+var colors;
+var maxSize = 26;
 
+var magenta = "#ff00ff";
 var red = "#ff0000";
 var orange = "#ffa500";
 var yellow = "#ffff00";
 var green = "#00ff00";
+var cyan = "#00ffff"
 var blue = "#0000ff";
 var purple = "#800080";
 
 function reset() {
-	moves = 25;
-	for (i = 1; i <= size; i++) {
-		for (j = 1; j <= size; j++) {
-			num = Math.ceil(Math.random()*6);
+	size = document.getElementById("size").value;
+	colors = document.getElementById("colors").value;
+	moves = Math.round(4.257*colors - 0.914 + (size/4-3.5)*1.189*colors);
+	for (i = 1; i <= 2; i++) {
+		for (j = 1; j <= 4; j++) {
+			circle = $("#top tr:nth-child(" + i.toString() + 
+				") td:nth-child(" + j.toString() + ")");
+			if (2 * (j-1) + i > colors)
+				circle.css("display", "none");
+			else
+				circle.css("display", "table-cell");
+		}
+	}
+	for (i = 1; i <= maxSize; i++) {
+		for (j = 1; j <= maxSize; j++) {
+			num = Math.ceil(Math.random()*colors);
 			if (num === 1) var color = red;
-			else if (num === 2) var color = orange;
-			else if (num === 3) var color = yellow;
-			else if (num === 4) var color = green;
-			else if (num === 5) var color = blue;
-			else if (num === 6) var color = purple;
-			getTableElement(i, j).css("background-color", color);
+			else if (num === 2) var color = green;
+			else if (num === 3) var color = blue;
+			else if (num === 4) var color = yellow;
+			else if (num === 5) var color = purple;
+			else if (num === 6) var color = orange;
+			else if (num === 7) var color = magenta;
+			else if (num === 8) var color = cyan;
+			if (i <= size && j <= size) {
+				getTableElement(i, j).css("display", "table-cell");
+				getTableElement(i, j).css("background-color", color);
+			}
+			else
+				getTableElement(i, j).css("display", "none");
 			getTableElement(i, j).attr("flooded", "no");
 			if (i ===1 && j ===1) var firstColor = color;
 		}
@@ -29,9 +52,10 @@ function reset() {
 }
 
 function rules() {
-	alert("Click a colored circle to change the color of the upper-left square \
-		and all connected squares of the same color. The object of the game is \
-		to fill the entire board with a single color.");
+	alert("Click a colored circle to change the color of the upper-\
+		left square and all connected squares of the same color. \
+		The object of the game is to fill the entire board with \
+		a single color.");
 }
 
 function flood(color) {
