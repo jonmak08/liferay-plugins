a40f4a07bc1d2759aded6b7f005b17e9a30c6f29
LAM_NGUYEN <LAM.NGUYEN@LIFERAY.COM>
2014-07-17 12:14:17 -0700

    latest_version intern theme project

--- themes/ward-theme/docroot/_diffs/js/main.js
+++ themes/ward-theme/docroot/_diffs/js/main.js
@@ -61,6 +61,7 @@ YUI({ lang: 'en' }).use(
   }
 );
 
+
 // Image cropper
 YUI().use(
   'aui-image-cropper',
@@ -73,49 +74,77 @@ YUI().use(
       }
     ).render();
 
-    var statusTPL = '<strong class="x">x: {x}</strong><strong class="y">y: {y}</strong><strong class="height">height: {height}</strong><strong class="width">width: {width}</strong>';
-
-    var updateImage = function() {
-      var cropRegion = imageCropper.get('region');
-
-      croppedImage.setStyles(
-        {
-          'backgroundPosition': (-cropRegion.x) + 'px ' + (-cropRegion.y) + 'px',
-          height: cropRegion.height,
-          width: cropRegion.width
-        }
-      );
-
-      croppedImage.html(Y.Lang.sub(statusTPL, cropRegion));
-    };
-
-    imageCropper.after(
-      'crop',
-      function(event) {
-        updateImage();
-      }
-    );
-
     var croppedImage = Y.one('#croppedImage');
 
-    var croppedStatus = Y.one('#croppedStatus');
+    if (croppedImage != null) {
 
-    croppedImage.show();
+        var statusTPL = '<strong class="x">x: {x}</strong><strong class="y">y: {y}</strong><strong class="height">height: {height}</strong><strong class="width">width: {width}</strong>';
 
-    updateImage();
+        var updateImage = function() {
+          var cropRegion = imageCropper.get('region');
 
-    Y.one('#cropLink').on(
-      'click',
-      function (event) {
-        imageCropper.setAttrs(
-          {
-            cropHeight: 125,
-            cropWidth: 390,
-            x: 45,
-            y: 5
+          croppedImage.setStyles(
+            {
+              'backgroundPosition': (-cropRegion.x) + 'px ' + (-cropRegion.y) + 'px',
+              height: cropRegion.height,
+              width: cropRegion.width
+            }
+          );
+
+          croppedImage.html(Y.Lang.sub(statusTPL, cropRegion));
+        };
+
+        imageCropper.after(
+          'crop',
+          function(event) {
+            updateImage();
           }
         );
-      }
-    );
+
+        
+
+        var croppedStatus = Y.one('#croppedStatus');
+
+        croppedImage.show();
+
+        updateImage();
+
+        Y.one('#cropLink').on(
+          'click',
+          function (event) {
+            imageCropper.setAttrs(
+              {
+                cropHeight: 125,
+                cropWidth: 390,
+                x: 45,
+                y: 5
+              }
+            );
+          }
+        );
+      } // end if
   }
 );
+
+
+// Scroll to top button
+$(function(){
+ 
+    $(document).on( 'scroll', function(){
+ 
+        if ($(window).scrollTop() > 100) {
+            $('.scroll-top-wrapper').addClass('show');
+        } else {
+            $('.scroll-top-wrapper').removeClass('show');
+        }
+    });
+ 
+    $('.scroll-top-wrapper').on('click', scrollToTop);
+}); 
+function scrollToTop() {
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
