6c8328bc6ab82a22d4a36fd6bb607cd8cbb6ff28
LAM_NGUYEN <LAM.NGUYEN@LIFERAY.COM>
2014-07-17 18:28:01 -0700

    updates

--- themes/ward-theme/docroot/_diffs/js/main.js
+++ themes/ward-theme/docroot/_diffs/js/main.js
@@ -62,70 +62,67 @@ YUI({ lang: 'en' }).use(
 );
 
 
-// Image cropper
-YUI().use(
-  'aui-image-cropper',
-  function(Y) {
-    var imageCropper = new Y.ImageCropper(
-      {
-        srcNode: '#image',
-        x: 100,
-        y: 100
-      }
-    ).render();
+// // Image cropper
+// YUI().use(
+//   'aui-image-cropper',
+//   function(Y) {
+//     var imageCropper = new Y.ImageCropper(
+//       {
+//         srcNode: '#image',
+//         x: 100,
+//         y: 100
+//       }
+//     ).render();
 
-    var croppedImage = Y.one('#croppedImage');
+//     var croppedImage = Y.one('#croppedImage');
 
-    if (croppedImage != null) {
+//     if (croppedImage != null) {
 
-        var statusTPL = '<strong class="x">x: {x}</strong><strong class="y">y: {y}</strong><strong class="height">height: {height}</strong><strong class="width">width: {width}</strong>';
+//         var statusTPL = '<strong class="x">x: {x}</strong><strong class="y">y: {y}</strong><strong class="height">height: {height}</strong><strong class="width">width: {width}</strong>';
 
-        var updateImage = function() {
-          var cropRegion = imageCropper.get('region');
+//         var updateImage = function() {
+//           var cropRegion = imageCropper.get('region');
 
-          croppedImage.setStyles(
-            {
-              'backgroundPosition': (-cropRegion.x) + 'px ' + (-cropRegion.y) + 'px',
-              height: cropRegion.height,
-              width: cropRegion.width
-            }
-          );
+//           croppedImage.setStyles(
+//             {
+//               'backgroundPosition': (-cropRegion.x) + 'px ' + (-cropRegion.y) + 'px',
+//               height: cropRegion.height,
+//               width: cropRegion.width
+//             }
+//           );
 
-          croppedImage.html(Y.Lang.sub(statusTPL, cropRegion));
-        };
+//           croppedImage.html(Y.Lang.sub(statusTPL, cropRegion));
+//         };
 
-        imageCropper.after(
-          'crop',
-          function(event) {
-            updateImage();
-          }
-        );
+//         imageCropper.after(
+//           'crop',
+//           function(event) {
+//             updateImage();
+//           }
+//         );        
 
-        
+//         var croppedStatus = Y.one('#croppedStatus');
 
-        var croppedStatus = Y.one('#croppedStatus');
+//         croppedImage.show();
 
-        croppedImage.show();
+//         updateImage();
 
-        updateImage();
-
-        Y.one('#cropLink').on(
-          'click',
-          function (event) {
-            imageCropper.setAttrs(
-              {
-                cropHeight: 125,
-                cropWidth: 390,
-                x: 45,
-                y: 5
-              }
-            );
-          }
-        );
-      } // end if
-  }
-);
-
+//         Y.one('#cropLink').on(
+//           'click',
+//           function (event) {
+//             imageCropper.setAttrs(
+//               {
+//                 cropHeight: 125,
+//                 cropWidth: 390,
+//                 x: 45,
+//                 y: 5
+//               }
+//             );
+//           }
+//         );
+//       } // end if
+//   }
+// );
 
 // Scroll to top button
 $(function(){
@@ -148,3 +145,48 @@ function scrollToTop() {
     offsetTop = offset.top;
     $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
 }
+
+// carousel
+YUI().use(
+  'aui-carousel',
+  function(Y) {
+    new Y.Carousel(
+      {
+        activeIndex: 'rand',
+        contentBox: '#myCarousel',
+        height: 300,
+        intervalTime: 5,
+        width: 1200
+      }
+    ).render();
+  }
+);
+
+
+// tabview
+YUI().use(
+  'aui-tabview',
+  function(Y) {
+    new Y.TabView(
+      {
+        children: [
+          {
+              content: '<br><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dicta aspernatur beatae fuga neque cupiditate laudantium itaque pariatur deleniti tenetur modi voluptatem animi sunt eligendi nisi corporis expedita quaerat facilis.</p>',
+              label: 'Tab #1'
+          },
+          {
+              content: '<br><p>Doloremque beatae rem voluptate nulla perspiciatis atque laudantium nihil impedit molestiae fuga veritatis quibusdam nam maiores aliquid. Deserunt dolorum quas temporibus enim ex nihil nemo perspiciatis. Nisi deserunt rem id pariatur in nostrum?</p>',
+              label: 'Tab #2'
+          },
+          {
+              content: '<br><p>Aliquid ipsum asperiores alias temporibus autem impedit soluta ut id iure explicabo veritatis consectetur debitis eaque recusandae odit quas nobis maxime saepe. Incidunt amet obcaecati. Ducimus soluta unde repellat laboriosam fuga modi rem itaque!</p>',
+              label: 'Tab #3'
+          }
+       ],
+       srcNode: '#myTab',
+       stacked: true,
+       type: 'pills'
+      }
+    ).render();
+  }
+);
