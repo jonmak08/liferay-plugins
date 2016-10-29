f6c93d0f138b37e36378b64e52ac11de5df425b5
Benjamin Johnson <benjamin.johnson@liferay.com>
2016-10-28 17:40:04 -0700

    Port Theme

---
+++ themes/port-theme/src/WEB-INF/src/resources-importer/journal/templates/Banner/Full_Page_Banner.ftl
@@ -0,0 +1,7 @@
+<div class="full-page-banner jumbotron photo-bg" style="background-image: url(${BackgroundImage.getData()})">
+	<div class="banner-content">
+		<h1>${Heading.getData()}</h1>
+
+		<h4>${Subheading.getData()}</h4>
+	</div>
+</div>
---
+++ themes/port-theme/src/js/main.js
@@ -0,0 +1,60 @@
+AUI().ready(
+
+	/*
+	This function gets loaded when all the HTML, not including the portlets, is
+	loaded.
+	*/
+
+	function() {
+		var $banner = AUI.$('#banner');
+		var $document = AUI.$(document);
+		var $firstPortlet = AUI.$('#content').find('.portlet-boundary').first();
+
+		var $mainContent = AUI.$('#main-content').attr('class');
+		var singleColumnFormat = $mainContent.indexOf('column-1') >= 0 || $mainContent.indexOf('columns-1') >= 0;
+
+		function toggleBannerFill() {
+			$banner.toggleClass('filled', $document.scrollTop() > 150);
+		}
+
+		if ($firstPortlet.hasClass('portlet-banner') && singleColumnFormat) {
+
+			toggleBannerFill();
+
+			$document.scroll(
+				function() {
+					toggleBannerFill();
+				}
+			);
+		}
+		else {
+			$banner.addClass('filled');
+			AUI.$('#content').addClass('content-under-header');
+		}
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	node: the Alloy Node object of the current portlet
+	*/
+
+	function(portletId, node) {
+	}
+);
+
+Liferay.on(
+	'allPortletsReady',
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
